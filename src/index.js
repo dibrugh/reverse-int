6236df5174b02cdb18056f25fea729784caae386
dibrugh <dibrugh@gmail.com>
2023-03-03 17:34:11 +0300

    feat: task has been done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    let i = 0;
+    let stringNumber = String(Math.abs(n));
+    while (i < stringNumber.length) {
+        result = stringNumber[i] + result;
+        i++;
+    }
+    return +result;
 }
